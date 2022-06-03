26b2f855e28ebe516d4c11836b39efff62c309e2
agvan <agvandyke25@gmail.com>
2022-06-03 18:22:36 -0500

    changed styles and created event listener and fetch

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 /*OMDB js*/
 
-var omdbAPIKey = "5385144e";
+ // "5385144e";
 
-$("#play").click(function(){
-    //action goes here!
 
+// Gets the data from the OMDB API and sets the content that will be displayed in the #result div
+$("#submit").click(function(){
+
+fetch("http://www.omdbapi.com/?apikey=5385144e&s")
+.then(function(success){
+    success.json()
+})
+.then(function(movies){
+    console.log(movies)
+})
+.catch(function (error){ console.log(error)});
 });
