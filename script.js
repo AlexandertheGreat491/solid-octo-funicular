5a7c2ce2fd41f83385a201d81136b57b08c4e64b
agvan <agvandyke25@gmail.com>
2022-06-04 00:08:05 -0500

    fetch request working now

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 /*OMDB js*/
 
-fetch('http://www.omdbapi.com/?apikey=5385144e&s')
-.then(function(success){success.json()})
-.then(function(movies){console.log(movies)})
-.catch(function(error){console.log(error)})
+var requestUrl = "https://www.omdbapi.com/?apikey=5385144e&t=Batman&plot=full";
+
+fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
+
+
+
 
 
 
