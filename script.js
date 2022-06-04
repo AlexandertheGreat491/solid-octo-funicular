5817847f86f08ac87409871983ff2611f7f03868
agvan <agvandyke25@gmail.com>
2022-06-03 19:05:06 -0500

    changed concation of url and added script element for materialize

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 /*OMDB js*/
 
- // "5385144e";
-
-
 // Gets the data from the OMDB API and sets the content that will be displayed in the #result div
 $("#submit").click(function(){
 
-fetch("http://www.omdbapi.com/?apikey=5385144e&s")
+fetch("http://www.omdbapi.com/?apikey=5385144e&t" + title + "&y=" + year + "&plot=short&r=json")
 .then(function(success){
     success.json()
 })
