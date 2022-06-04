1bc281f22784d29648fa23598b45e3f577886852
agvan <agvandyke25@gmail.com>
2022-06-04 12:49:27 -0500

    changed ids & added global variables

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 /*OMDB js*/
+// Global variables
+var userForm = document.querySelector('#user-form');
+var movieTitle = document.querySelector('#enter-title');
+var movieYear = document.querySelector('#year');
+var moviePlot = document.querySelector('#plot-choice');
+var responseType = document.querySelector('#response');
+
+// Batman
 
 var requestUrl = "https://www.omdbapi.com/?apikey=5385144e&t=Batman&plot=full";
 
@@ -10,6 +18,17 @@ fetch(requestUrl)
     console.log(data);
   });
 
+
+// Joker
+var requestUrl = "https://www.omdbapi.com/?apikey=5385144e&t=Joker&plot=full";
+
+fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
 
 
 
