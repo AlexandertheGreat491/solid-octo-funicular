1153aba2b1de1daf371c55e3462315599402c4d7
agvan <agvandyke25@gmail.com>
2022-06-03 23:10:55 -0500

    reworked the js

--- script.js
+++ script.js
@@ -1,34 +1,10 @@
 /*OMDB js*/
 
+fetch('http://www.omdbapi.com/?apikey=5385144e&s')
+.then(function(success){success.json()})
+.then(function(movies){console.log(movies)})
+.catch(function(error){console.log(error)})
 
-document.getElementById("getForm").addEventListener("submit", function(event){
-    event.preventDefault();
-    loadMovies(document.querySelector("input[name='query'").value);
-});
-
-function loadMovies(name) {
-    var omdbAPI = new XMLHttpRequest();
-    var omdbURL = 'http://www.omdbapi.com/?apikey=5385144e&t=${name.replace(" ", "%20")}&type=movie';
-    omdbAPI.open("get", omdbURL, true);
-    omdbAPI.onload = function(event) {
-        event.preventDefault();
-        if(this.status == 200) {
-            var result = JSON.parse(this.responseText);
-            console.log(result);
-            var output = "";
-            for (var i in result) {
-                output +=
-                '<div class="user">' +
-                '<h3>Title: ' + result.Title + '</h3>' +
-                '</div>'; 
-            }
-            document.getElementById('result').innerHTML = output;
-        } else {
-            alert("No results");
-        }
-    }
-    omdbAPI.send();
-}
 
 
 
