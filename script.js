031480fb86ca89c9c8bb2dc3224c37e2fc3f8b6b
agvan <agvandyke25@gmail.com>
2022-06-03 22:52:01 -0500

    tried another approach

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 /*OMDB js*/
 
-// Gets the data from the OMDB API and sets the content that will be displayed in the #result div
- 
-$("#submit").click(function(){
-    var queryUrl = "http://www.omdbapi.com/?apikey=5385144e&t" + title + "&y=" + year + "&plot=short&r=json"
-    fetch(queryUrl)
-    .then(function(response){
-        return response.json();
-    })
-})
+
+document.getElementById("getForm").addEventListener("submit", function(event){
+    event.preventDefault();
+    loadMovies(document.querySelector("input[name='query'").value);
+});
+
+function loadMovies(name) {
+    var omdbAPI = newXMLHttpRequest();
+    var omdbURL = ("http://www.omdbapi.com/?apikey=5385144e&t" + title + "&y=" + year + "&plot=short&r=json");
+    omdbAPI.open("get", omdbURL, true);
+    omdbAPI.onload = function(event) {
+        event.preventDefault();
+        if(this.status == 200) {
+            var result = JSON.parse(this.responseText);
+            console.log(result);
+            var output = "";
+            for (var i in result) {
+                output +=
+                '<div class="user">' +
+                '<h3>Title: ' + result.Title + '</h3>' +
+                '</div>'; 
+            }
+            document.getElementById('result').innerHTML = output;
+        } else {
+            alert("No results");
+        }
+    }
+    omdbAPI.send();
+}
 
 
 
