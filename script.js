80ee16de89cbbeb5ce993006172bb2ec0563d260
agvan <agvandyke25@gmail.com>
2022-06-04 16:41:12 -0500

    added a border-radius to <article> border, created fetch, it's working but showing errors, check parameters

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ var moviesContainerEl = document.querySelector('#movies-container');
 var movieSearchTerm = document.querySelector('#movie-search-term');
 var submitButtonEl = document.querySelector('#submit');
 var errorMessageEl = document.createElement("p");
+var getUserMoviesErrorEl = document.createElement("p");
 
 var sumbitSearchHandler = function (event) {
     event.preventDefault();
@@ -23,20 +24,33 @@ var sumbitSearchHandler = function (event) {
         moviesContainerEl.textContent = '';
         movieTitleValue.value = '';
         errorMessageEl.textContent = '';
+        console.log(movieTitleValue);
     } else {
-        //console.log("errorhere");
-        errorMessageEl.textContent = 'Please enter a valid movie title';
+        console.log("errorhere");
+        errorMessageEl.textContent = 'Please enter a valid movie title.';
         moviesContainerEl.appendChild(errorMessageEl);
     }
 
 }
 
+// Requests results from the OMDB API for the user.
 
-//getUserMovies
-var getUserMovies = function() {
+var getUserMovies = function(title, year, plot, json, xml) {
+    var myapiUrl = ("http://www.omdbapi.com/?apikey=5385144e&t" + title + "&y=" + year + plot + "&r=" + json + xml);
+    // OMDB API fetch request
+    fetch(myapiUrl).then(function (response){
+if (response.ok) {
+    console.log(response);
+    response.json().then(function (data){
+        console.log(data);
+        //display(data, title, year, plot, json, xml);
+    });
+} else {
+    getUserMoviesErrorEl.textContent = 'Sorry, we were unable to retrieve your search results.';
+}
+    })
 
 }
-
 
 // Submit button event listener
 submitButtonEl.addEventListener('click', sumbitSearchHandler);
