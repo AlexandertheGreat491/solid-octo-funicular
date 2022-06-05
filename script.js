8380f2a680881f4655df769eda4ebad35eba9b38
agvan <agvandyke25@gmail.com>
2022-06-05 12:59:08 -0500

    created displayMovies function

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ var movieSearchTerm = document.querySelector('#movie-search-term');
 var submitButtonEl = document.querySelector('#submit');
 var errorMessageEl = document.createElement("p");
 var getUserMoviesErrorEl = document.createElement("p");
+var displayMoviesEl = document.createElement("p");
+
 
 var sumbitSearchHandler = function (event) {
     event.preventDefault();
@@ -30,62 +32,108 @@ var sumbitSearchHandler = function (event) {
         errorMessageEl.textContent = 'Please enter a valid movie title.';
         moviesContainerEl.appendChild(errorMessageEl);
     }
-
+    if (movieYearValue) {
+        getUserMovies(movieYearValue);
+        movieYearEl.textContent = '';
+        movieYearValue.value = '';
+        errorMessageEl.textContent = '';
+        console.log(movieYearValue);
+    } else {
+        console.log("errorhere");
+        errorMessageEl.textContent = 'Please enter a valid year.';
+        movieYearEl.appendChild(errorMessageEl);
+    }
+    if (moviePlotValue) {
+        getUserMovies(moviePlotValue);
+        moviePlotEl.textContent = '';
+        moviePlotValue.value = '';
+        errorMessageEl.textContent = '';
+        console.log(moviePlotValue);
+    } else {
+        console.log("errorhere");
+        errorMessageEl.textContent = 'Please enter valid plot type.';
+    }
+    if (responseTypeValue) {
+        getUserMovies(responseTypeValue);
+        responseTypeEl.textContent = '';
+        responseTypeValue.value = '';
+        errorMessageEl.textContent = '';
+        console.log(responseTypeValue);
+    } else {
+        console.log("errorhere");
+        errorMessageEl.textContent = 'Please enter a valid response type: JSON or XML.'
+    }
+    
 }
 
 
 // Requests results from the OMDB API for the user.
-var getUserMovies = function(movieTitleValue) {
-    var myapiUrl = "http://www.omdbapi.com/?apikey=5385144e&s + title";
-    // OMDB API fetch request
-    fetch(myapiUrl).then(function (response){
-if (response.ok) {
-    console.log(response);
-    response.json().then(function (data){
-        console.log(data);
-        displayMovies(data.items, movieTitleValue);
-    });
-} else {
-    getUserMoviesErrorEl.textContent = 'Sorry, we were unable to retrieve your search results.';
-}
+var getUserMovies = function (movieTitleValue) {
+    var myapiUrl = "http://www.omdbapi.com/?apikey=be881b6b&/?s/?i";
+    fetch(myapiUrl).then(function (response) {
+        if (response.ok) {
+            console.log(response);
+            response.json().then(function (data) {
+                console.log(data);
+                displayMovies(data.items, movieTitleValue);
+            });
+        } else {
+            getUserMoviesErrorEl.textContent = 'Sorry, we were unable to retrieve your search results.';
+        }
     });
 
 };
 
-var getUserMovies = function(movieYearValue) {
-var myapiUrl = "http://www.omdbapi.com/?apikey=5385144e&y + year"
-fetch(myapiUrl).then(function (response){
-    if (response.ok) {
-console.log(response);
-response.json().then(function(data){
-    console.log(data);
-    displayMovies(data.items, movieYearValue);
-}); 
-    } else {
-        getUserMoviesErrorEl.textContent = 'Sorry, we were unable to retrieve your search results.';
-    }
-});
+var getUserMovies = function (movieYearValue) {
+    var myapiUrl = "http://www.omdbapi.com/?apikey=be881b6b&y + year";
+    fetch(myapiUrl).then(function (response) {
+        if (response.ok) {
+            console.log(response);
+            response.json().then(function (data) {
+                console.log(data);
+                displayMovies(data.items, movieYearValue);
+            });
+        } else {
+            getUserMoviesErrorEl.textContent = 'Sorry, we were unable to retrieve your search results.';
+        }
+    });
 };
 
 var getUserMovies = function(moviePlotValue) {
-var myapiUrl = "http://www.omdbapi.com/?apikey5385144e&plot + plot + short + full"
+var myapiUrl = "http://www.omdbapi.com/?apikeybe881b6b&plot + plot + short + full";
 fetch(myapiUrl).then(function (response){
-    if (response.ok) {
-        console.log(response);
-        response.json().then(function(data){
-            console.log(data);
-            displayMovies(data.items,moviePlotValue);
-                });
-    } else {
-        getUserMoviesErrorEl.textContent = 'Sorry, we were unable to retrieve your search results.'
-    }
-})
+if (response.ok) {
+console.log(response);
+response.json().then(function(data){
+console.log(data);
+displayMovies(data.items, moviePlotValue);
+ });
+} else {
+getUserMoviesErrorEl.textContent = 'Sorry, we were unable to retrieve your search results.'
+ }
+});
+};
+
+var getUserMovies = function(responseTypeValue) {
+var myapiUrl = "http://www.omdapi.com/?apikey5385144e&r";
+fetch(myapiUrl).then(function (response){
+if (response.ok) {
+ console.log(response);
+response.json().then(function(data){
+console.log(data);
+displayMovies(data.items, responseTypeValue);
+});
+} else {
+getUserMoviesErrorEl = 'Sorry, we were unable to retrieve your search results.'
 }
+});
+};
 
-
-//var displayMovies = function(movieTitleValue) {
-
-//}
+var displayMovies = function(movieTitleValue, movieSearchTerm) {
+if (movieTitleValue === '') {
+displayMoviesEl = movieSearchTerm;
+}
+}
 
 // Submit button event listener
 submitButtonEl.addEventListener('click', sumbitSearchHandler);
